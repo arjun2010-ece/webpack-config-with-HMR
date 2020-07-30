b2bed1e7e2d8c0cfef2040754266c53590ca5de6
Arjun sah <arjun2010.ece@gmail.com>
2020-07-30 03:08:05 +0100

    third commit working

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -5,7 +5,7 @@ const path = require("path");
 const config = {
     entry: ["react-hot-loader/patch", "./src/index.js"],
     output: {
-        path: path.resolve(__dirname, "src/index.js"),
+        path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
     },
     module: {
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,11 +1,12 @@
 const { merge } = require('webpack-merge');
 const base = require("./webpack.base.config.js");
+var path = require("path");
 
 
 const config = {
     devServer: {
         port: 8080,
-        contentBase: "./dist"
+        contentBase: path.join(__dirname, "dist"),
     }
 }
 
